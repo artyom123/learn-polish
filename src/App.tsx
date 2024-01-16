47d56326874e80442d480402dc9382038ee6ba88
Artsem_Virshych <artsem_virshych@epam.com>
2024-01-16 21:39:17 +0200

    test: comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Home from './pages/home/home.tsx'
 
-// eslint-disable-next-line no-console
-console.log('=====================>>>> ', process.env.REACT_APP_TEST);
+// // eslint-disable-next-line no-console
+// console.log('=====================>>>> ', process.env.REACT_APP_TEST);
 
 function App() {
   return <Home />
